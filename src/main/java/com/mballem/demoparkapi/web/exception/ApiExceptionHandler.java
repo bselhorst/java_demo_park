cb8fca5183c4b30b565e43af1bc0a01d3d856778
Bruno Selhorst <bselhorst88@gmail.com>
2024-10-27 12:41:48 -0500

    atualizacoes

---
+++ src/main/java/com/mballem/demoparkapi/web/exception/ApiExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.mballem.demoparkapi.web.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.mballem.demoparkapi.exception.UsernameUniqueViolationException;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class ApiExceptionHandler {
+    @ExceptionHandler(UsernameUniqueViolationException.class)
+    public ResponseEntity<ErrorMessage> methodArgumentNotValidException(RuntimeException ex, HttpServletRequest request){
+        log.error("Api Error - ", ex);
+        return ResponseEntity
+        .status(HttpStatus.CONFLICT)
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(new ErrorMessage(request, HttpStatus.CONFLICT, "Campos inválidos"));
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessage> methodArgumentNotValidException(MethodArgumentNotValidException ex, HttpServletRequest request, BindingResult result){
+        log.error("Api Error - ", ex);
+        return ResponseEntity
+        .status(HttpStatus.UNPROCESSABLE_ENTITY)
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(new ErrorMessage(request, HttpStatus.UNPROCESSABLE_ENTITY, "Campos inválidos", result));
+    }
+}
